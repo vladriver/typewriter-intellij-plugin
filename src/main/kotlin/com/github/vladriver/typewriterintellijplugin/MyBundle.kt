bc3210dc154b1da7789916f859ea068b07823b54
GitHub Action <action@github.com>
2021-10-16 19:25:50 +0000

    Template cleanup

--- src/main/kotlin/com/github/vladriver/typewriterintellijplugin/MyBundle.kt
+++ src/main/kotlin/com/github/vladriver/typewriterintellijplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.vladriver.typewriterintellijplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/vladriver/typewriterintellijplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.vladriver.typewriterintellijplugin.services
+
+import com.github.vladriver.typewriterintellijplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
